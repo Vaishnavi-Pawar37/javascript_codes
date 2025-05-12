76a2931aec2709ce3af72425f4d939f1cf2705ce
Vaishnavi-Pawar37 <145538313+Vaishnavi-Pawar37@users.noreply.github.com>
2025-05-12 12:20:23 +0530

    edited code

--- class.js
+++ class.js
@@ -2,5 +2,11 @@ class student{
     student_data(){
         console.log("I am Student");
     }
+    student_info()
+    {
+        name = "Sai";
+        age = 12;
+        console.log(`Name of stuent is ${name} and age of that student is ${age} years old.`);
+    }
 }
 let stud = new student();
